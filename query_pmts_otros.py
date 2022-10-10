87bcfc9ed99422ec7709599988346e22b71c16b0
luisvillarreal <34928905+luisvillarreal@users.noreply.github.com>
2022-10-10 11:48:00 -0500

    Update query_pmts_otros.py

--- query_pmts_otros.py
+++ query_pmts_otros.py
@@ -13,6 +13,7 @@ company_address.zip_code AS `company_address.zip_code`,
 company_address.country AS `company_address.country`,
 company_address.address AS `company_address.address`,
 company_address.city AS `company_address.city`,
+company.razon_social AS `company.razon_social`,
 sum(pago_efectuado.importe) AS `pago_efectuado.importe`
 FROM pago_efectuado
 INNER JOIN pago_procesado_emp ON pago_efectuado.id = pago_procesado_emp.pago_efectuado_id
@@ -38,5 +39,6 @@ pago_procesado_emp.referencia_alphanumerica,
 company_address.zip_code,
 company_address.country,
 company_address.address,
-company_address.city
+company_address.city,
+company.razon_social
 '''
